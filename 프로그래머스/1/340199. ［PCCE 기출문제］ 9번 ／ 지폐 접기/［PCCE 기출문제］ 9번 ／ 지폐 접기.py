b9b9984216b4131cf43d6527956d5ac210b1baa4
Sohee <dev.sh.kor@gmail.com>
2025-03-25 10:00:10 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
@@ -0,0 +1,10 @@
+def solution(wallet, bill):
+    answer = 0
+    while 1:
+        if min(wallet) >= min(bill) and max(wallet) >= max(bill): 
+            return answer
+        else:
+            if bill[0] >= bill[1]:
+                bill[0] //= 2 
+            else: bill[1] //= 2
+            answer += 1
